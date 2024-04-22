a4476b4224b9659cb329e7d0634f43be2ca89a55
Yasutake Tomoya <ty0108.98@icloud.com>
2024-04-22 22:39:39 +0900

    section8_J01-08_array_and_reference_type

---
+++ src/Sample1_08_1.java
@@ -0,0 +1,18 @@
+class Sample1_08_1 {
+	public static void main (String[] args) {
+		
+		//配列の活用
+		
+		char[] singou = new char[3] ;
+		singou[0] = '赤' ;
+		singou[1] = '黄' ;
+		singou[2] = '青' ;
+		
+		char x = singou[1] ;
+		System.out.println("要素の参照：" + x );
+		
+		int count = singou.length ;
+		System.out.println("要素数：" + count );
+		
+	}
+}
